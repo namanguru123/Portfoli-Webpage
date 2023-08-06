67212237cd2e6b90e0e77eb3e0b2f9cb6a6d1791
namanguru123 <86717425+namanguru123@users.noreply.github.com>
2023-08-06 22:06:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+let home = document.querySelector("#home");
+let about = document.querySelector("#about-me");
+let project = document.querySelector("#projects");
+let exp = document.querySelector("#experience");
+let contact = document.querySelector("#Contact");
+
+let input = document.querySelector("input");
+
+input.addEventListener("change", function(){
+    if(input.value == "About" || "about" || "About Me" || "about-me"){
+        window.scroll({
+            top:700,
+            behavior : "smooth",
+        })
+    }
+
+    else if(input.value == "Projects" || "projects" || "Project"){
+        window.scroll({
+            top:1350,
+            behavior : "smooth",
+        })
+
+    }
+})
+
+home.addEventListener("click", function(event){
+    event.preventDefault();
+    window.scroll({
+        top:0,
+        behavior : "smooth",
+    })
+})
+
+
+about.addEventListener("click", function(event){
+    event.preventDefault();
+    window.scroll({
+        top:700,
+        behavior : "smooth",
+    })
+})
+
+project.addEventListener("click", function(event){
+    event.preventDefault();
+    window.scroll({
+        top:1350,
+        behavior : "smooth",
+    })
+})
+
+exp.addEventListener("click", function(event){
+    event.preventDefault();
+    window.scroll({
+        top:2100,
+        behavior : "smooth",
+    })
+})
+
+contact.addEventListener("click", function(event){
+    event.preventDefault();
+    window.scroll({
+        top:2500,
+        behavior : "smooth",
+    })
+})
